c15e5d0853d19251f672d7342026204ba9279632
chihiro <chanchi0707@gmail.com>
2017-12-10 22:49:52 +0900

    とりあえず押したら音なるようにした

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,19 @@
-import $ from 'jquery';
-import login from "./components/login.js";
-// import character_action from './components/character_action.js';
-// こっからスタート
+
+
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM読み込んだよ');
-
+  let target = document.getElementById('handBell__sound');
+  console.log(target);
+  target.addEventListener('click', sound, false);
+  function sound() {
+  	let sound = document.getElementById('sound-file');
+  	sound.play();
+  	sound.currentTime = 0 ;
+  	// スマホに対応するために[currentTime]を認識した場合のみ、処理をする
+	if( typeof( sound.currentTime ) != 'undefined' )
+	  {
+	    sound.currentTime = 0;
+	  }
+	}
 });
 

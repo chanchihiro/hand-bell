e8613dcad116eaf7bd3ffc0e01413c162f651e6c
chihiro <chanchi0707@gmail.com>
2017-12-11 03:31:05 +0900

    色と同時に音も切り替わる

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -2,73 +2,77 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  var bell_target = document.getElementById('handBell__sound');
-  var button = document.getElementById('btn');
+		var bell_target = document.getElementById('handBell__sound');
+		var button = document.getElementById('btn');
+		var soundData = document.getElementById('sound-file');
 
-  function sound() {
-    var sound = document.getElementById('sound-file');
-    sound.play();
-    sound.currentTime = 0;
-    // スマホに対応するために[currentTime]を認識した場合のみ、処理をする
-    if (typeof sound.currentTime != 'undefined') {
-      sound.currentTime = 0;
-    }
-  }
+		function sound() {
+				soundData.play();
+				soundData.currentTime = 0;
+				// スマホに対応するために[currentTime]を認識した場合のみ、処理をする
+				if (typeof sound.currentTime != 'undefined') {
+						soundData.currentTime = 0;
+				}
+		}
 
-  function change_color() {
-    if (bell_target.classList.contains('handBell__sectionRed')) {
-      bell_target.classList.add('handBell__sectionBlue');
-      bell_target.classList.remove('handBell__sectionRed');
-    } else if (bell_target.classList.contains('handBell__sectionBlue')) {
-      bell_target.classList.add('handBell__sectionYellow');
-      bell_target.classList.remove('handBell__sectionBlue');
-    } else if (bell_target.classList.contains('handBell__sectionYellow')) {
-      bell_target.classList.add('handBell__sectionBlack');
-      bell_target.classList.remove('handBell__sectionYellow');
-    } else if (bell_target.classList.contains('handBell__sectionBlack')) {
-      bell_target.classList.add('handBell__sectionWhite');
-      bell_target.classList.remove('handBell__sectionBlack');
-    } else if (bell_target.classList.contains('handBell__sectionWhite')) {
-      bell_target.classList.add('handBell__sectionPurple');
-      bell_target.classList.remove('handBell__sectionWhite');
-    } else if (bell_target.classList.contains('handBell__sectionPurple')) {
-      bell_target.classList.add('handBell__sectionGreen');
-      bell_target.classList.remove('handBell__sectionPurple');
-    } else if (bell_target.classList.contains('handBell__sectionGreen')) {
-      bell_target.classList.add('handBell__sectionAzure');
-      bell_target.classList.remove('handBell__sectionGreen');
-    } else if (bell_target.classList.contains('handBell__sectionAzure')) {
-      bell_target.classList.add('handBell__sectionRed');
-      bell_target.classList.remove('handBell__sectionAzure');
-    }
-  }
+		function change_color_sound() {
+				if (bell_target.classList.contains('handBell__sectionRed')) {
+						bell_target.classList.add('handBell__sectionBlue');
+						bell_target.classList.remove('handBell__sectionRed');
+						soundData = document.getElementById('sound-file2');
+				} else if (bell_target.classList.contains('handBell__sectionBlue')) {
+						bell_target.classList.add('handBell__sectionYellow');
+						bell_target.classList.remove('handBell__sectionBlue');
+						soundData = document.getElementById('sound-file');
+				} else if (bell_target.classList.contains('handBell__sectionYellow')) {
+						bell_target.classList.add('handBell__sectionBlack');
+						bell_target.classList.remove('handBell__sectionYellow');
+						soundData = document.getElementById('sound-file2');
+				} else if (bell_target.classList.contains('handBell__sectionBlack')) {
+						bell_target.classList.add('handBell__sectionWhite');
+						bell_target.classList.remove('handBell__sectionBlack');
+						soundData = document.getElementById('sound-file');
+				} else if (bell_target.classList.contains('handBell__sectionWhite')) {
+						bell_target.classList.add('handBell__sectionPurple');
+						bell_target.classList.remove('handBell__sectionWhite');
+						soundData = document.getElementById('sound-file2');
+				} else if (bell_target.classList.contains('handBell__sectionPurple')) {
+						bell_target.classList.add('handBell__sectionGreen');
+						bell_target.classList.remove('handBell__sectionPurple');
+						soundData = document.getElementById('sound-file');
+				} else if (bell_target.classList.contains('handBell__sectionGreen')) {
+						bell_target.classList.add('handBell__sectionAzure');
+						bell_target.classList.remove('handBell__sectionGreen');
+						soundData = document.getElementById('sound-file2');
+				} else if (bell_target.classList.contains('handBell__sectionAzure')) {
+						bell_target.classList.add('handBell__sectionRed');
+						bell_target.classList.remove('handBell__sectionAzure');
+						soundData = document.getElementById('sound-file');
+				}
+		}
 
-  bell_target.addEventListener('click', sound, false);
-  btn.addEventListener('click', change_color, false);
+		bell_target.addEventListener('click', sound, false);
+		btn.addEventListener('click', change_color_sound, false);
 
-  //加速度の部分
-  window.addEventListener("devicemotion", function (event) {
-    var x = parseFloat(event.acceleration.x);
-    var y = parseFloat(event.acceleration.y);
-    var z = parseFloat(event.acceleration.z);
-    document.getElementById('acc-x').textContent = x;
-    document.getElementById('acc-y').textContent = y;
+		//加速度の部分
+		window.addEventListener("devicemotion", function (event) {
+				var x = parseFloat(event.acceleration.x);
+				var y = parseFloat(event.acceleration.y);
+				var z = parseFloat(event.acceleration.z);
+				document.getElementById('acc-x').textContent = x;
+				document.getElementById('acc-y').textContent = y;
 
-    // 横に振ったらベルが鳴る
-    if (x > 4) {
-      sound();
-    }
-    // 奥に振ったら色が変わる
-    if (y > 8) {
-      change_color();
-    }
-    // アイフォンの向きをアンドロイドに揃える
-    if (userAgent.indexOf("iPhone") > 0 || userAgent.indexOf("iPad") > 0 || userAgent.indexOf("iPod") > 0) {
-      x *= -1;
-      y *= -1;
-      z *= -1;
-    }
-  });
+				// 横に振ったらベルが鳴る
+				if (x > 4) {
+						sound();
+				}
+				// アイフォンの向きをアンドロイドに揃える
+				if (userAgent.indexOf("iPhone") > 0 || userAgent.indexOf("iPad") > 0 || userAgent.indexOf("iPod") > 0) {
+						x *= -1;
+						y *= -1;
+						z *= -1;
+				}
+		});
 });
 
 },{}]},{},[1]);
--- src/js/index.js
+++ src/js/index.js
@@ -2,47 +2,55 @@
 document.addEventListener('DOMContentLoaded', function() {
   let bell_target = document.getElementById('handBell__sound');
   let button = document.getElementById('btn');
+  let soundData = document.getElementById('sound-file');
 
   function sound() {
-  	let sound = document.getElementById('sound-file');
-  	sound.play();
-  	sound.currentTime = 0 ;
+  	soundData.play();
+  	soundData.currentTime = 0 ;
   	// スマホに対応するために[currentTime]を認識した場合のみ、処理をする
 	if( typeof( sound.currentTime ) != 'undefined' ){
-	    sound.currentTime = 0;
+	    soundData.currentTime = 0;
 	}
   }
 
-  function change_color() {
+  function change_color_sound() {
 	if(bell_target.classList.contains('handBell__sectionRed')) {
 	  bell_target.classList.add('handBell__sectionBlue');
 	  bell_target.classList.remove('handBell__sectionRed');
+	  soundData = document.getElementById('sound-file2');
 	} else if(bell_target.classList.contains('handBell__sectionBlue')){
 	  bell_target.classList.add('handBell__sectionYellow');
 	  bell_target.classList.remove('handBell__sectionBlue');
+	  soundData = document.getElementById('sound-file');
 	} else if(bell_target.classList.contains('handBell__sectionYellow')){
 	  bell_target.classList.add('handBell__sectionBlack');
 	  bell_target.classList.remove('handBell__sectionYellow');
+	  soundData = document.getElementById('sound-file2');
 	} else if(bell_target.classList.contains('handBell__sectionBlack')){
 	  bell_target.classList.add('handBell__sectionWhite');
 	  bell_target.classList.remove('handBell__sectionBlack');
+	  soundData = document.getElementById('sound-file');
 	} else if(bell_target.classList.contains('handBell__sectionWhite')){
 	  bell_target.classList.add('handBell__sectionPurple');
 	  bell_target.classList.remove('handBell__sectionWhite');
+	  soundData = document.getElementById('sound-file2');
 	} else if(bell_target.classList.contains('handBell__sectionPurple')){
 	  bell_target.classList.add('handBell__sectionGreen');
 	  bell_target.classList.remove('handBell__sectionPurple');
+	  soundData = document.getElementById('sound-file');
 	} else if(bell_target.classList.contains('handBell__sectionGreen')){
 	  bell_target.classList.add('handBell__sectionAzure');
 	  bell_target.classList.remove('handBell__sectionGreen');
+	  soundData = document.getElementById('sound-file2');
 	} else if(bell_target.classList.contains('handBell__sectionAzure')){
 	  bell_target.classList.add('handBell__sectionRed');
 	  bell_target.classList.remove('handBell__sectionAzure');
+	  soundData = document.getElementById('sound-file');
 	}
   }
 
   bell_target.addEventListener('click', sound, false);
-  btn.addEventListener('click', change_color, false);
+  btn.addEventListener('click', change_color_sound, false);
 
   //加速度の部分
   window.addEventListener("devicemotion", function(event) {
@@ -55,10 +63,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // 横に振ったらベルが鳴る
     if(x > 4) {
     	sound();
-    }
-    // 奥に振ったら色が変わる
-    if(y > 8) {
-    	change_color();
     }
     // アイフォンの向きをアンドロイドに揃える
     if (userAgent.indexOf("iPhone") > 0 || userAgent.indexOf("iPad") > 0 || userAgent.indexOf("iPod") > 0) {
